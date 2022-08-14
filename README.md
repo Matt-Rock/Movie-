d6dcc3fda16cb69e7e1894738bfb49c2e09160d6
toribio59 <mtterazarce@hotmail.com>
2022-08-14 23:51:24 +0200

    new design, everything deleted to start over

--- README.md
+++
@@ -1,7 +0,0 @@
-Version 1.0 Of Movie App
-
-This is a beginner project for react beginner developper , it will use an api to show featured movies,
-if it doesn't work, it's because it will need and api key, you will have to ask themoviedb for a key and change the current
-in ./misc/apifunctions , API_KEY
-
-Try now: https://movies-app-xi.vercel.app
--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { apiGet } from './misc/apiFunctions';
-import Card from './components/Card';
-import Search from './components/Search';
 
-const App = () => {
-  const [data, setData] = useState([]);
 
-  useEffect(() => {
-    apiGet().then(r => setData(r.results));
-  }, []);
-
-  const updateData = string => {
-    apiGet(string).then(r => {
-      console.log(r.results);
-      return setData(r.results);
-    });
-  };
-
-  return (
-    <>
-      <Search setData={updateData} />
-      <div className="movies-container">
-        {data.length > 0 &&
-          data.map(movie => <Card key={movie.id} {...movie} />)}
-      </div>
-    </>
-  );
-};
+function App() {
+  return <div>Hello</div>
+}
 
 export default App;
